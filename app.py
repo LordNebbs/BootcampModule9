5482e21d66fd6a5de96f6897229004f1ffb05be8
LordNebbs <106819085+LordNebbs@users.noreply.github.com>
2023-01-02 18:13:56 -0500

    save changes

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 #import Flask from sqlalchemy
 from flask import Flask, jsonify
+
 
 #setup database. This allows us to access and query the database
 engine = create_engine("sqlite:///hawaii.sqlite")
@@ -19,3 +20,22 @@ Base.prepare(engine, reflect=True)
 
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+
+#create session link from python to database
+session = Session(engine)
+
+#set up Flask
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+    return
